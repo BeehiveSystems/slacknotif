db6304c6bc897b18c6e71b57c22e9bda6464a13a
Leo <ohayon.leo@gmail.com>
2017-09-22 22:33:26 +0200

    Add post function.

--- slacknotif.py
+++ slacknotif.py
@@ -1,21 +1,25 @@
+import socket
 import io
 import shutil
+import requests
+import json
 
-print("Be sure to run this as sudo/root. Press enter to continue...")
+key = input("Be sure to run this as root/sudo. Press enter to continue.")
+webhook = input("Paste your slack webhook url: ")
 
-url = input("Paste your slack webhook url: ")
 filename = "slacknotif"
-script = """#!/bin/bash
-	function post_to_slack () {
-	       	SLACK_USERNAME="$(hostname)"
- 	     	SLACK_MESSAGE="$1"
-     		SLACK_URL=$SLACK_URL
-	        curl -sSL --data "payload={\"username\": \"${SLACK_USERNAME}\", \"text\": \"${SLACK_ICON} ${SLACK_MESSAGE}\"}" ${SLACK_URL}
-	}
-	post_to_slack "$(cat)"
-	exit 0""".encode()
 
-with io.FileIO("slacknotif", "w") as file:
+def post_to_slack():
+    slack_username = print(socket.gethostname())
+    slack_message = print("Test.")
+    slack_url = webhook
+    payload = {'username:' : 'slack_username' : 'text' : "slack_message"}
+    r = requests.get('https://hooks.slack.com/services/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', params=payload)
+
+post_to_slack()
+
+
+with io.FileIO("%s" % filename, "w") as file:
     file.write(script)
 
 shutil.move("./slacknotif", "/opt/%s" % filename)
