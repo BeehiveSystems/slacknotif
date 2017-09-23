bed3f29ed241872ee31a36caa99a161c43292346
Leo <ohayon.leo@gmail.com>
2017-09-23 22:49:30 +0200

    Corrected errors, reworked formatting.

--- slacknotif.py
+++ slacknotif.py
@@ -6,20 +6,17 @@ import json
 
 key = input("Be sure to run this as root/sudo. Press enter to continue.")
 webhook = input("Paste your slack webhook url: ")
-
 filename = "slacknotif"
-
-def post_to_slack():
-    slack_username = print(socket.gethostname())
-    slack_message = print("Test.")
-    slack_url = webhook
-    payload = {'username:' : 'slack_username' : 'text' : "slack_message"}
-    r = requests.get('%s' % webhook, params=payload)
-
-post_to_slack()
-
 
 with io.FileIO("%s" % filename, "w") as file:
     file.write(script)
-
 shutil.move("./slacknotif", "/opt/%s" % filename)
+
+def post_to_slack():
+    slack_username = socket.gethostname()
+    slack_message = print("Test.")
+    slack_url = webhook
+    payload = {'username:' : 'slack_username', 'text' : "slack_message"}
+    r = requests.get('%s' % webhook, params=payload)
+
+post_to_slack()
