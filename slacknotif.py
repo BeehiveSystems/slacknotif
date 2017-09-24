87392092c8d795890ce3ab15a6e3f50d91bdad19
Leo <ohayon.leo@gmail.com>
2017-09-24 12:25:08 +0200

    Added creation of slacknotif file and moving it to /opt/ directory.

--- slacknotif.py
+++ slacknotif.py
@@ -8,13 +8,18 @@ key = input("Be sure to run this as root/sudo. Press enter to continue.")
 webhook = input("Paste your slack webhook url: ")
 filename = "slacknotif"
 
-with io.FileIO("%s" % filename, "w") as file:
-    file.write(script)
-shutil.move("./slacknotif", "/opt/%s" % filename)
-
 def post_to_slack():
     slack_username = socket.gethostname()
     slack_message = "Test."
     payload = {'username': slack_username, 'text': slack_message,}
     requests.post(webhook, json=payload)
 post_to_slack()
+
+with io.FileIO("%s" % filename, "w") as file:
+    file.write("def post_to_slack():".encode())
+    file.write("    slack_message = \"Test.\"\n".encode())
+    file.write("    slack_username = socket.gethostname()\n".encode())
+    file.write("    payload = {'username': slack_username, 'text': slack_message,}\n".encode())
+    file.write("    requests.post(webhook, json=payload)\n".encode())
+    file.write("post_to_slack()\n".encode())
+shutil.move("./slacknotif", "/opt/%s" % filename)
