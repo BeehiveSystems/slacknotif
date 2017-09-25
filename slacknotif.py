733e5fcf47ba7f7f6263a80e0a92a9530ce65ef6
Leo <ohayon.leo@gmail.com>
2017-09-25 21:53:49 +0200

    Added cron support for yum and apt systems.

--- slacknotif.py
+++ slacknotif.py
@@ -6,7 +6,7 @@ import os
 key = input("Be sure to run this as root/sudo. Press enter to continue.")
 webhook = input("Paste your slack webhook url: ").encode()
 filename = "slacknotif"
-system = input("Are you using a Debian or RHEL based system?").lower()
+system = input("Are you using a Debian or RHEL based system? ").lower()
 
 with io.FileIO("%s" % filename, "w") as file:
     file.write(
@@ -27,19 +27,19 @@ post_to_slack()""".encode() % webhook)
 shutil.move("./slacknotif", "/opt/%s" % filename)
 os.chmod("/opt/%s" % filename, 0o755)
 
-print("Slacknotif has been installed successfully.")
-
-if system = debian:
+if system == "debian":
+	print("Adding cron updates for apt.")
 	with io.FileIO("/etc/cron.d/slack", "w") as file:
 		file.write(
 			"""SHELL=/bin/sh
 			PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-			30 8 * * * root /usr/bin/apt-get update && /usr/bin/apt-get install unattended-upgrades | /opt/%s""" % filename)
-if system = rhel:
+			30 8 * * * root /usr/bin/apt-get update && /usr/bin/apt-get install unattended-upgrades | /opt/%(filename)""".encode())
+elif system == "rhel":
+	print("Adding cron updates for yum.")
 	with io.FileIO("/etc/cron.d/slack", "w") as file:
 		file.write( 
 			"""SHELL=/bin/sh
 			PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-			30 8 * * * root /usr/bin/yum update | /opt/%s""" % filename)
+			30 8 * * * root /usr/bin/yum update | /opt/%(filename)""".encode())
 else:
 	print("Unrecognized system type.")
